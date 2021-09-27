52882fd506cc00e3418660514c0980a214f5e01c
loSoSh <loic@soundshape.io>
2021-09-27 17:26:14 +0200

    added link and mouseover color change to the footer

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const test = document.getElementById("test");
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
+const codecademi = document.getElementById("codecademy")
 
-test.onclick = function() {
-    test.style.color = "#" + randomColor;
-}
+test.addEventListener('mouseenter', () => {
+    test.style.color = "#882d2d";
+});
+
+test.addEventListener('mouseleave', () => {
+    test.style.color = "#f1d8c1";
+});
+
+codecademy.addEventListener('mouseenter', () => {
+    test.style.color = rgb(61, 21, 21);
+});
+
+codecademy.addEventListener('mouseleave', () => {
+    test.style.color = "#882d2d";
+});
